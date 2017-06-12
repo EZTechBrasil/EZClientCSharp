d42f421efc29c99ad7172db9663f1a4bc408f69e
ericeztech <eric@eztech.ind.br>
2017-06-12 17:16:33 -0300

    Add file Version.cs

---
+++ Version.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EZClientCSharp
+{
+    class Version
+    {
+        public const string VERSION	= "2,0,0,0" ;
+        public const string EZSERVER_VERSION_COMPATIBILITY = "2,3,0,1" ;
+    }
+}
